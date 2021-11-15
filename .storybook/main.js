e2f8c907760346ec0cb90642742016cf60fa0ae0
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2021-11-15 15:46:29 +0530

    fix: :bug: custom filter selection (#515)
    * fix: :green_heart: storybook build
    
    * fix: bug: selection issues with custom filter

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,12 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-knobs",
     "@storybook/addon-links",
+    {
+      name: "storybook-addon-turbo-build",
+      options: {
+        optimizationLevel: 2,
+      },
+    },
   ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
